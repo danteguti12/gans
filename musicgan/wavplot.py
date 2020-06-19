4b562dcbf55f248ddf969a31e218e1dcec2a8394
Dante Gutierrez Palma <danteguti12@gmail.com>
2020-06-19 00:32:19 -0500

    Update wavplot.py

--- musicgan/wavplot.py
+++ musicgan/wavplot.py
@@ -4,16 +4,20 @@ from pydub import AudioSegment
 from os import path
 import os
 
-for file in os.listdir("/home/danteguti12/Documents/pyprojects/Musicplot/mp3"):
+file_dir = "/mydirectory"
+temp_dir = "/temp"
+plots_dir = "/plots"
+
+for file in os.listdir(file_dir):
 	if file.endswith(".mp3"):
 	
-		src = "/home/danteguti12/Documents/pyprojects/Musicplot/mp3/" + file
-		dst = "/home/danteguti12/Documents/pyprojects/Musicplot/wav/" + os.path.splitext(file)[0] + '.wav'
+		src = file_dir + file
+		dst = temp_dir + os.path.splitext(file)[0] + '.wav'
 		sound = AudioSegment.from_mp3(src)
 		sound.export(dst, format="wav")
 
 		samplingFreq, signalData = wavfile.read(dst)
 		signalData = signalData[:,0]
 		plt.specgram(signalData, Fs=samplingFreq)
-		plt.savefig("/home/danteguti12/Documents/pyprojects/Musicplot/graphs/" + os.path.splitext(file)[0] + '.png')
+		plt.savefig(plots_dir + os.path.splitext(file)[0] + '.png')
 		os.remove(dst)
