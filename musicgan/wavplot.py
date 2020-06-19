8f3d404579e60dea0b7ff9223a021c3e621da709
Dante Gutierrez Palma <danteguti12@gmail.com>
2020-06-19 00:36:08 -0500

    Update wavplot.py

--- musicgan/wavplot.py
+++ musicgan/wavplot.py
@@ -4,20 +4,23 @@ from pydub import AudioSegment
 from os import path
 import os
 
-file_dir = "/mydirectory"
-temp_dir = "/temp"
-plots_dir = "/plots"
+#this script locates all your MP3 files in a specified directory
+#the temp directory will store temporary wave files generated to produce the final plot
+
+file_dir = "/mydirectory" #mp3 files directory
+temp_dir = "/temp" #temporary wave file directory
+plots_dir = "/plots" #plots directory
 
 for file in os.listdir(file_dir):
 	if file.endswith(".mp3"):
-	
+
 		src = file_dir + file
 		dst = temp_dir + os.path.splitext(file)[0] + '.wav'
-		sound = AudioSegment.from_mp3(src)
-		sound.export(dst, format="wav")
+		sound = AudioSegment.from_mp3(src) 
+		sound.export(dst, format="wav") #convert to wave file
 
-		samplingFreq, signalData = wavfile.read(dst)
-		signalData = signalData[:,0]
-		plt.specgram(signalData, Fs=samplingFreq)
-		plt.savefig(plots_dir + os.path.splitext(file)[0] + '.png')
-		os.remove(dst)
+		samplingFreq, signalData = wavfile.read(dst) #read wave file
+		signalData = signalData[:,0] #select 0 for left channel 
+		plt.specgram(signalData, Fs=samplingFreq) #plot spectrogram
+		plt.savefig(plots_dir + os.path.splitext(file)[0] + '.png') #save file
+		os.remove(dst) #remove temporary wave file
